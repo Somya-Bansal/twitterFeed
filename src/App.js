ee4b5fb07ed2963db5208cde7806be8ee4805ca5
Somya Bansal <sobansal@deloitte.com>
2019-11-08 10:47:52 +0530

    search feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import React, { Component } from 'react';
-import './App.scss';
 
-import Search from './components/Search/search';
+import './App.scss';
 import Card from './components/Card/card';
 
 class App extends Component {
-  state = {
-    cards: []
+
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      cards: [],
+      searchKey: ''
+    }
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+  componentDidMount(){
+    if(window.location.search){
+      let url = 'https://aravindtwitter.herokuapp.com/twittersearch' + window.location.search
+      this.getData(url);
+    }
+    console.log(window.location.search);
   }
 
-  componentDidMount(){
-    // fetch('https://aravindtwitter.herokuapp.com/twittersearch?key=adobe')
-    fetch('https://aravindtwitter.herokuapp.com/twittersearch')
+  getData(url){
+    this.setState({
+      loading: true
+    })
+    fetch(url)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({
+        cards: data.statuses,
+        loading: false
+      })
+    })
+  }
+
+  handleSearch() {
+    let url = 'https://aravindtwitter.herokuapp.com/twittersearch?key=' + this.state.searchKey;
+    this.getData(url);
+  }
+
+  handleSearchKey = ev => {
+    this.setState({
+      searchKey: ev.target.value
+    });
   }
 
   render() {
+    const searchKey = this.state.searchKey;
     return (
       <div className="app">
-        <Search></Search>
-        <Card></Card>
+        <div className="SearchContainer">
+          <div className="SearchHeading">
+            <span className="searchAtText">Search @ Twitter</span>
+            <span className="autoRefreshText">
+              Auto refresh in 27 seconds
+          </span>
+          </div>
+          <div className="SearchBarContainer">
+            <div className="SearchBar">
+              <input
+                onChange={(ev) => this.handleSearchKey(ev)}
+                value={searchKey}
+                type="text"
+                placeholder="Search"
+              >
+              </input>
+            </div>
+            <div className="SearchButton">
+              <button
+                onClick={this.handleSearch}
+              >Search</button>
+            </div>
+          </div>
+        </div>
+        <Card cards={this.state.cards} loading={this.state.loading}></Card>
       </div>
     );
   }
--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,28 +1,60 @@
 import React from 'react';
-import avatar from '../../images/my-pic.jpg'
 
 import './card.scss';
 
 class Card extends React.Component {
   render() {
     return (
-      <div>
-        <div className="Card">
-          <div className="cardAvatar">
-              <img src={avatar} alt="" />
-          </div>
-          <div className="cardInfo">
-            <div className="cardInfoProfile">
-              <div className="profileUsername">Somya Bansal</div>
-              <div className="profileHandle">@somya</div>
-              <div className="timestamp">Wed Jul 25 01:23:32 +0000 2018</div>
+      <div className="feed">
+        {this.props.loading ? 
+          <>
+            <h2>
+              LOADING...
+            </h2>
+          </>
+        :
+          this.props.cards.map((card) => (
+            <div className="Card" key={card.id}>
+              <div className="cardAvatar">
+                <img src={card.user.profile_image_url_https} alt="" />
+              </div>
+              <div className="cardInfo">
+                <div className="cardInfoProfile">
+                  <div className="profileUsername">{card.user.name}</div>
+                  <div className="profileHandle">{card.user.screen_name}</div>
+                  <div className="timestamp">{card.created_at}</div>
+                </div>
+                <div className="cardInfoTweet">{card.text}</div>
+              </div>
             </div>
-            <div className="cardInfoTweet">Some tweet</div>
-          </div>
-        </div>
+          ))
+        }
       </div>
     );
   }
 }
+// const Card = ({ cards }) => {
+//   console.log(cards);
+//   return (
+//     <div>
+//       {cards.map((card) => (
+//         <div className="Card" key={card.id}>
+//           <div className="cardAvatar">
+//             <img src={card.user.profile_image_url_https} alt="" />
+//           </div>
+//           <div className="cardInfo">
+//             <div className="cardInfoProfile">
+//               <div className="profileUsername">{card.user.name}</div>
+//               <div className="profileHandle">{card.user.screen_name}</div>
+//               <div className="timestamp">{card.created_at}</div>
+//             </div>
+//             <div className="cardInfoTweet">{card.text}</div>
+//           </div>
+//         </div>
+//       ))
+//       }
+//     </div>
+//   );
+// }
 
 export default Card;
